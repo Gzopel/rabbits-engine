b2730ff0653370b7562a3e1caf23009aee819bb8
g <jepunto@gmail.com>
2016-09-28 21:04:03 -0300

    Character collision

--- tests/ActionApplier.test.js
+++ tests/ActionApplier.test.js
@@ -4,69 +4,40 @@ import eventEmitter from 'event-emitter';
 import { ACTIONS } from '../lib/rules/rules';
 import ActionApplier from '../lib/rules/ActionApplier';
 
+const axeGuy = require('./testData/axeGuy.json');
+const archer = require('./testData/archer.json');
+
 describe(__filename, () => {
-  const characterOne = {
-    id:1,
-    position: { x: 0, z: 1 },
-    health: 7,
-    items: {
-      weapon: {
-        range: 1,
-        type:'AXE',
-        damage: 4,
-        difficulty: 7,
-      },
-    },
-    sheet: {
-      attributes: {
-        physical: {
-          strength: 2,
-          dexterity: 2,
-          stamina: 3,
-        },
-      },
-      abilities: {
-        talents: {},
-        skills: {
-          melee: 2,
-        },
-      },
-      maxHealth: 7,
-    },
-  };
-  const characterTwo = {
-    id:2,
-    position: { x: 0, z: 2 },
-    health: 7,
-    items: {
-      weapon: {
-        range: 20,
-        type:'BOW',
-        damage: 7,
-        difficulty: 7,
-      },
-    },
-    sheet: {
-      attributes: {
-        physical: {
-          dexterity: 2,
-          stamina: 3,
-        },
-      },
-      abilities: {
-        talents: {
-          athletics: 2,
-          dodge: 2,
-        },
-        skills: {
-          archery: 2,
-        },
-      },
-      maxHealth: 7,
-    },
-  };
-  const characters = { 1: characterOne, 2: characterTwo };
+  describe('Move collition', () => {
+    const characterOne =JSON.parse(JSON.stringify(axeGuy))
+    const characterTwo = JSON.parse(JSON.stringify(archer));
+    const characters = { 1: characterOne, 2: characterTwo };
+    const emitter = eventEmitter();
+    const applier = new ActionApplier(emitter, characters);
+
+    it('should collide instantly if next to each other', (done) => {
+      const testFn = (update) => {
+        assert(update.character === characterTwo.id, 'Should update character two');
+        assert(update.result === 'collision', 'Should collide');
+        assert(characterTwo.position.z === archer.position.z, 'Should not increase z position');
+        assert(characterTwo.position.x === archer.position.x, 'Should not increase x position');
+        emitter.off('characterUpdate', testFn);
+        done();
+      };
+      emitter.on('characterUpdate', testFn);
+      emitter.emit('newState', {
+        action: ACTIONS.WALKING,
+        owner: characterTwo.id,
+        direction: { x: 0, z: 0},
+      });
+    });
+
+  });
+
   describe('Epic fight!', () => {
+    const characterOne = JSON.parse(JSON.stringify(axeGuy))
+    const characterTwo = JSON.parse(JSON.stringify(archer));
+    const characters = { 1: characterOne, 2: characterTwo };
     const emitter = eventEmitter();
     const applier = new ActionApplier(emitter, characters);
 
@@ -81,8 +52,8 @@ describe(__filename, () => {
         emitter.off('characterUpdate', testFn);
         done();
       };
-      emitter.on('characterUpdate',testFn);
-      emitter.emit('newState',{
+      emitter.on('characterUpdate', testFn);
+      emitter.emit('newState', {
         action: ACTIONS.BASIC_ATTACK,
         owner: characterOne.id,
         target: characterTwo.id,
@@ -90,17 +61,16 @@ describe(__filename, () => {
     });
 
     it('2. Archer should flee', (done) => {
-      const archerOldZ = characterTwo.position.z;
       const testFn = (update) => {
         assert(update.character === characterTwo.id, 'Should update character two');
-        assert(update.result === 'walk');
-        assert(characterTwo.position.z > archerOldZ, 'Should increase z position');
-        assert(characterTwo.position.x === 0, 'Should not increase x position');
+        assert(update.result === 'walk', 'should be walking');
+        assert(characterTwo.position.z > archer.position.z, 'Should increase z position');
+        assert(characterTwo.position.x === archer.position.x, 'Should not increase x position');
         emitter.off('characterUpdate', testFn);
         done();
       };
-      emitter.on('characterUpdate',testFn);
-      emitter.emit('newState',{
+      emitter.on('characterUpdate', testFn);
+      emitter.emit('newState', {
         action: ACTIONS.WALKING,
         owner: characterTwo.id,
         direction: { x: 0, z: 20},
@@ -118,8 +88,8 @@ describe(__filename, () => {
         emitter.off('characterUpdate', testFn);
         done();
       };
-      emitter.on('characterUpdate',testFn);
-      emitter.emit('newState',{
+      emitter.on('characterUpdate', testFn);
+      emitter.emit('newState', {
         action: ACTIONS.BASIC_ATTACK,
         owner: characterTwo.id,
         target: characterOne.id,
@@ -127,17 +97,16 @@ describe(__filename, () => {
     });
 
     it('4. Axe should try to hit but move instead', (done) => {
-      const axeOldZ = characterOne.position.z;
       const testFn = (update) => {
         assert(update.character === characterOne.id, 'Should update character one');
-        assert(update.result === 'walk');
-        assert(characterOne.position.z > axeOldZ, 'Should increase z position');
-        assert(characterOne.position.x === 0, 'Should not increase x position');
+        assert(update.result === 'walk', 'Should be walking');
+        assert(characterOne.position.z > axeGuy.position.z, 'Should increase z position');
+        assert(characterOne.position.x === axeGuy.position.x, 'Should not increase x position');
         emitter.off('characterUpdate', testFn);
         done();
       };
-      emitter.on('characterUpdate',testFn);
-      emitter.emit('newState',{
+      emitter.on('characterUpdate', testFn);
+      emitter.emit('newState', {
         action: ACTIONS.BASIC_ATTACK,
         owner: characterOne.id,
         target: characterTwo.id,
