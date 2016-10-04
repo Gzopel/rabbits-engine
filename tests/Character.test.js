f5fedbb8b753cf0dba48357b90f1bf56899a28ec
g <jepunto@gmail.com>
2016-10-04 04:49:03 -0300

    character class enhancements

--- tests/Character.test.js
+++ tests/Character.test.js
@@ -5,7 +5,7 @@ const axeGuy = require('./testData/archer.json');
 
 describe(__filename, () => {
   it('Should create a new character that wraps the character sheet', () => {
-    const character = new Character(axeGuy.sheet);
+    const character = new Character(1, axeGuy.sheet);
     assert.equal(axeGuy.sheet.attributes.physical.dexterity, character.getBase('attributes.physical.dexterity'));
     assert.equal(axeGuy.sheet.attributes.physical.stamina, character.getBase('attributes.physical.stamina'));
     assert.equal(axeGuy.sheet.attributes.mental.perception, character.getBase('attributes.mental.perception'));
@@ -18,7 +18,7 @@ describe(__filename, () => {
     const path = 'attributes.physical.dexterity';
     modificators.set(path,
       [{ mod: 2, ttl: timestamp, source: 1 }, { mod: 2, ttl: timestamp + 100, source: 2 }]);
-    const character = new Character(axeGuy.sheet, modificators);
+    const character = new Character(1, axeGuy.sheet, { x: 0, z: 0 }, modificators);
     assert.equal(axeGuy.sheet.attributes.physical.dexterity + 4, character.get(path));
     character.updateModifiers(timestamp + 50);
     assert.equal(axeGuy.sheet.attributes.physical.dexterity + 2, character.get(path));
@@ -28,8 +28,8 @@ describe(__filename, () => {
     assert.equal(axeGuy.sheet.attributes.physical.dexterity + 1, character.get(path));
     character.addModifiers({ path: path, mod: 2, ttl: timestamp, source: 2 });
     assert.equal(axeGuy.sheet.attributes.physical.dexterity + 3, character.get(path));
-    character.addModifiers({ path: path, mod: 1, ttl: timestamp, source: 2 });
-    assert.equal(axeGuy.sheet.attributes.physical.dexterity + 2, character.get(path));
+    character.addModifiers({ path: path, mod: -2, ttl: timestamp, source: 2 });
+    assert.equal(axeGuy.sheet.attributes.physical.dexterity - 1, character.get(path));
   });
 
 
